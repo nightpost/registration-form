1b1584a39442d86125c094243ecf881edcbf132f
Andrey Sverkunov <andrey.sverkunov@citi.com>
2019-08-05 00:40:59 +0300

    add first error for input fields. add tests

--- src/app/modules/auth/containers/registrationForm/registrationForm.component.ts
+++ src/app/modules/auth/containers/registrationForm/registrationForm.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { InputType, IPasswordCriteria } from '../../models';
+import { FormControl, FormGroup, NgForm, ValidationErrors } from '@angular/forms';
 import { EmailValidator, PasswordValidator } from '../../../../validators';
-import { PasswordErrorType } from '../../../../models';
+import { IFieldCriteria, InputType } from '../../models';
+import { PasswordErrorType, EmailErrorType } from '../../../../models';
 
 @Component({
   selector: 'as-registration-form',
@@ -19,15 +19,28 @@ export class RegistrationFormComponent implements OnInit {
 
   readonly emailInputField: string = 'email';
   readonly passwordInputField: string = 'password';
+  private readonly firstIndex = 0;
 
   form: FormGroup;
-  passwordCriteria: IPasswordCriteria[] = [
+  emailCriteria: IFieldCriteria[] = [
+    { mapping: EmailErrorType.Required, description: 'Email is required*' },
+    { mapping: EmailErrorType.Pattern, description: 'Include "@" sign and at least 2 letters of domain*' }
+  ];
+  passwordCriteria: IFieldCriteria[] = [
     { mapping: PasswordErrorType.MinLength, description: '8+ characters' },
     { mapping: PasswordErrorType.MinLowercaseLength, description: 'lowercase letter' },
     { mapping: PasswordErrorType.MinUppercaseLength, description: 'uppercase letter' },
     { mapping: PasswordErrorType.MinNumberLength, description: 'number' },
     { mapping: PasswordErrorType.MinSpecialCharacterLength, description: 'special character' },
   ];
+
+  get emailInputControl(): FormControl {
+    return this.form.get(this.emailInputField) as FormControl;
+  }
+
+  get passwordInputControl(): FormControl {
+    return this.form.get(this.passwordInputField) as FormControl;
+  }
 
   ngOnInit(): void {
     this.form = this.buildForm();
@@ -40,7 +53,21 @@ export class RegistrationFormComponent implements OnInit {
     });
   }
 
-  onSubmit(): void {
-    console.log('Submit form', this.form);
+  getFirstError(errors: ValidationErrors, inputField: string): string {
+    const error = Object.keys(errors)[this.firstIndex];
+    if (inputField === this.passwordInputField) {
+      const selectedCriterion = this.passwordCriteria.find((criterion: IFieldCriteria) => criterion.mapping === error);
+      return `Please check "${selectedCriterion.description}" rule*`;
+    } else if (inputField === this.emailInputField) {
+      const selectedCriterion = this.emailCriteria.find((criterion: IFieldCriteria) => criterion.mapping === error);
+      return selectedCriterion.description;
+    }
+  }
+
+  onSubmit(form: NgForm): void {
+    if (form.valid) {
+      form.resetForm({});
+    }
+    console.log(this.form);
   }
 }
